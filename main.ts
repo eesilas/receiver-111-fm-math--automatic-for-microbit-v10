80e0856b3b1f303c5c53306a658acb66038f4777
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2025-03-06 10:33:48 +0800

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 10) {
         basic.showIcon(IconNames.Sad)
+        microIoT.microIoT_showColor(0xffff00)
     } else if (receivedNumber == 11) {
         basic.showIcon(IconNames.Confused)
+        microIoT.microIoT_showColor(0x00ffff)
     } else {
         basic.showIcon(IconNames.No)
     }
